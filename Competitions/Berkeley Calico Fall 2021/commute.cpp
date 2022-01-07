510ae19055a8b6b750f5efa516808778a56a56c6
avnithv <avnith.v@gmail.com>
2022-01-07 00:14:00 +0000

    Added Calico competition

--- Competitions/Berkeley Calico Fall 2021/commute.cpp
+++ Competitions/Berkeley Calico Fall 2021/commute.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <bits/stdc++.h>
 #include <cmath>
-#include <complex>
 
 using namespace std;
 
@@ -12,12 +10,12 @@ typedef vector<bool> vb;
 typedef vector<vector<bool>> vvb;
 typedef pair<int, int> pii;
 typedef vector<pair<int, int>> vpii;
-typedef queue
-
-<int> qi;
+typedef queue<int> qi;
 typedef stack<int> si;
 typedef priority_queue<int> pqi;
 typedef priority_queue<int,vector<int>,greater<int>> pqmi;
+typedef vector<char> vc;
+typedef vector<pair<char, int>> vpci;
 
 struct ed {int s; int e; int w;};
 typedef vector<ed> ve;
@@ -26,9 +24,6 @@ struct nd {int i; vpii adjw;};
 typedef vector<nd> vn;
 typedef priority_queue<pii> pqii;
 typedef priority_queue<pii,vector<pii>,greater<pii>> pqmii;
-
-typedef complex<int> P;
-typedef vector<P> vp;
 
 #define FOR(i, a, b) for(int i = a; i < b; i++)
 #define FE(x, s) for(auto x : s)
@@ -40,29 +35,39 @@ typedef vector<P> vp;
 #define S second
 #define X real()
 #define Y imag()
+#define endl "\n"
 
-int dist(P p1, P p2) {
-  return abs(p2 - p1);
-}
-
-P mv(P p1, P p2) {
-  return {p2.X - p1.X, p2.Y - p1.Y};
-}
-
-double cross(P v1, P v2) {
-  return (conj(v1) * v2).Y;
-}
-
-double triangle_area(P p1, P p2, P p3) {
-  return cross(mv(p1, p2), mv(p1, p3)) /2.;
-}
-
-double shoelace(vp ps) {
-  int xs = ps.size();
-  double area = 0;
-  FOR(i, 0, xs) {
-    int x = (i + 1) % xs;
-    area += 1;
+int split(int n) {
+  vi x;
+  char c;
+  int min_num = -1;
+  int count = 0;
+  FOR(i, 0, n) {
+    string z = "";
+    cin.get(c);
+    while(c != ',' && c != '\n') {
+      if (48 <= c && c <= 57) z += c;
+      cin.get(c);
+    }
+    int num = stoi(z);
+    if (min_num < 0 || num < min_num) {
+      min_num = num;
+      count++;
+    }
   }
-  return abs(area) / 2;
+  return count;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  int ans = split(n);
+  cout << ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  FOR(i, 0, t) solve();
+  return 0;
 }
---
+++ Competitions/Berkeley Calico Fall 2021/desc.md
@@ -0,0 +1,4 @@
+## Competition Site:
+https://calico.berkeley.edu/
+## Problems
+https://calicojudge.com/domjudge/public/problems
